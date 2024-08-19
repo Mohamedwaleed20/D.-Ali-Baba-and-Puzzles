94b2ec3e3e0a7503b80883d2d939666d1a5022a1
Shagee3.eg2004 <168550015+Mohamedwaleed20@users.noreply.github.com>
2024-08-19 23:55:00 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+int main()
+{
+    long long a , b , c , d ;
+    cin >> a >> b >> c >> d ;
+    if(a-b+c==d)
+    {
+        cout << "YES" ;
+    }
+    else if(a+b-c==d)
+    {
+        cout << "YES" ;
+    }
+    else if(a+b*c==d)
+    {
+        cout << "YES" ;
+    }
+    else if(a*b+c==d)
+    {
+        cout << "YES" ;
+    }
+    else if(a*b-c==d)
+    {
+        cout << "YES" ;
+    }
+    else if(a-b*c==d)
+    {
+        cout << "YES" ;
+    }
+    else
+    {
+        cout << "NO" ;
+    }
+
+    return 0;
+}
